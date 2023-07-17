fc979f4c278233ace69452b8fd11f54684881671
Dmitry Gorokhov <gorokhov86@gmail.com>
2023-07-17 18:41:57 +0300

    add deploy host in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['webserver', 'localhost']
+ALLOWED_HOSTS = ['webserver', 'localhost', '.onrender.com']
 
 
 # Application definition
