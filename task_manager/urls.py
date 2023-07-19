62c3b4e5cfb318fd8500fb0af08ac062b75939c5
Dmitry Gorokhov <gorokhov86@gmail.com>
2023-07-19 22:15:25 +0300

    update templates, urls and views

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from task_manager import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.IndexView.as_view(), name='root'),
     path('admin/', admin.site.urls),
 ]
