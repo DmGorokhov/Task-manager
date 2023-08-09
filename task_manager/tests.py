fe8247d06dcfadb9913ab7a6bf576ee91d5047e8
Dmitry Gorokhov <gorokhov86@gmail.com>
2023-08-09 16:18:49 +0300

    update tests for TestHomePage, add check rendered fields

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -6,9 +6,38 @@ from task_manager.users.models import SiteUser
 
 
 class TestHomePage(TestCase, MixinForTests):
+    fixtures = ['db_users.json']
+
+    def setUp(self) -> None:
+        self.login_navbar_fields = [_('Statuses'),
+                                    _('Labels'),
+                                    _('Tasks'),
+                                    _('Logout')
+                                    ]
+        self.logout_navbar_fields = [_('Login'),
+                                     _('Sign Up'),
+                                     ]
 
     def test_homepage(self):
         self.assert_page('home', 'home.html')
+
+    def test_homepage_without_login(self):
+        response = self.client.get(reverse_lazy('home'))
+        for field in self.login_navbar_fields:
+            self.assertNotContains(response, field)
+
+        for field in self.logout_navbar_fields:
+            self.assertContains(response, field)
+
+    def test_homepage_with_login(self):
+        user = SiteUser.objects.get(pk=1)
+        self.client.force_login(user)
+        response = self.client.get(reverse_lazy('home'))
+        for field in self.login_navbar_fields:
+            self.assertContains(response, field)
+
+        for field in self.logout_navbar_fields:
+            self.assertNotContains(response, field)
 
 
 class Test_Login_Logout_Views(TestCase, MixinForTests):
