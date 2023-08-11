9e9f4d5edf0f48adfcd093a38f4c0c457861ce77
Dmitry Gorokhov <gorokhov86@gmail.com>
2023-08-11 15:18:05 +0300

    add mixin for tests

--- task_manager/mixins/mixins_for_tests.py
+++ task_manager/mixins/mixins_for_tests.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from django.contrib.messages import get_messages
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 
 
 class MixinForTests(TestCase):
@@ -15,3 +16,20 @@ class MixinForTests(TestCase):
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), message)
+
+    def _check_logout_redirect_and_flash(self, response):
+        error_message = _('You are logged out')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('login'))
+        self.assert_flashmessage(response, error_message)
+
+    def assert_logout_behavior(self, url_pattern, test_post_request=False,
+                               data_for_post=None):
+        self.client.logout()
+        response = self.client.get(reverse_lazy(url_pattern))
+        self._check_logout_redirect_and_flash(response)
+        if test_post_request:
+            response = self.client.post(reverse_lazy(url_pattern),
+                                        data_for_post)
+            self._check_logout_redirect_and_flash(response)
